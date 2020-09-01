31a098fd40e70e7af0c93c95f96186571119fa6e
danielj32 <danieljuanito21@hotmail.com>
2020-09-01 22:14:04 +0000

    add new task replace text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" listening port 5000 """
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_framework():
+    """ display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def disp_hbnb():
+    """ display /hbnb! """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def show_text(txt):
+    """ display /c/<text> """
+    return 'C %s' % txt.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run()
